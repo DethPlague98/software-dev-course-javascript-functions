a7aaf99a5e78aaae732df521ab796772e11fa28c
DethPlague98 <tnaes98@gmail.com>
2026-05-21 20:19:31 -0500

    class project

--- sharing-task.js
+++ sharing-task.js
@@ -1,3 +1,5 @@
+const readline = require('readline-sync');
+
 /*
 ===========================================
 🤝 Collaborative Coding Challenge: Event Helpers
@@ -37,6 +39,16 @@ reusable functions that solve specific tasks. This activity encourages:
 // 4. Return the result.
 
 
+    function nameRole(name,role){                   //declaring function
+        return "Name: " + name + ",  Role: " + role; 
+    }
+console.log(nameRole('Alice','Speaker'));      //assigning arguments && invoking function per assignment
+
+//below lets user assign their own name and role to the above function
+    let myName = readline.question("What is your name? ");
+    let myRole = readline.question("And what is your role? ");
+
+    console.log(nameRole(myName,myRole)); //uses custom answers given by the user to supply arguements to the function
 // ============================================
 // 🧩 Task 2: Calculate Event Cost
 // ============================================
@@ -51,6 +63,30 @@ reusable functions that solve specific tasks. This activity encourages:
 // 3. If so, apply a 10% discount.
 // 4. Return the final total.
 
+    function ticketIncome(count){
+        let tixCost = 20;
+        let total = 0;
+
+        for (i = 1; i <= count; i++){     
+            if (i <= 100){                  // loop for first 100 attendees
+                total = total + 20;
+            }
+        else if (count > 100){              //loop for overflow attendee adjusted ticket prices
+                total = total + 18;
+            }
+        }
+    return "Final ticket total: $" + total + ".00";
+
+    }
+console.log(ticketIncome(140));                                          //invoke function per assignment
+
+
+    //below is a little extra
+let incomePrediction = readline.question("How many people do you expect to attend this event? ");
+
+console.log("Predicted " + ticketIncome(incomePrediction));              //Predicts income based on given attendace
+
+
 
 // ============================================
 // 🧩 Task 3: Validate Email
@@ -63,6 +99,23 @@ reusable functions that solve specific tasks. This activity encourages:
 // Steps:
 // 1. Check if the string includes both "@" and ".".
 // 2. Return true or false accordingly.
+
+function ifValidEmail(email){
+    return email.includes("@")&&email.includes(".");   //checks if argument includes both the "@" and "." symbols & returns either true or false.
+}
+
+console.log(ifValidEmail("tnaes@yahoo.com")); //invokes function per assignment
+
+//Below checks if user's email is valid and responds.
+let userEmail = readline.question("Enter your email (Don't worry it's okay): ");
+
+if(ifValidEmail(userEmail) === true){
+    console.log("Your email is valid!!!");
+}
+else{
+    console.log("Your email isn't even an email :/// \nTry again dood");
+}
+
 
 
 // ============================================
